4821610c848b70498c13a93add98c612ac9a4eb2
Tahjour <tahjallman64@gmail.com>
2023-01-15 21:54:39 -0500

    changed some styling for the app page and buttons

--- components/layout/app/main-bar-menu.js
+++ components/layout/app/main-bar-menu.js
@@ -1,12 +1,22 @@
-import { BsPlusCircle } from "react-icons/bs";
+import { useState } from "react";
+import { BsGrid, BsPlusCircle } from "react-icons/bs";
+import { FaList } from "react-icons/fa";
 import styles from "./main-bar-menu.module.css";
 
 function MainBarMenu() {
+    const [listStyle, setListStyle] = useState("tile");
+
+    function changeListStyle() {
+        listStyle === "tile" ? setListStyle("list") : setListStyle("tile");
+    }
     return (
         <section className={styles.mainBarMenu}>
             <button className={styles.mainBarAddNewBtn}>
                 <BsPlusCircle className={styles.mainBarAddNewIcon} />
                 Add Item
+            </button>
+            <button className={styles.listStyleBtn} onClick={changeListStyle}>
+                {listStyle === "tile" ? <FaList className={styles.listStyleIcon} /> : <BsGrid className={styles.listStyleIcon} />}
             </button>
         </section>
     );
