2bf9195ad167752f0c65fe2a21ebb3fead745400
Tahjour <tahjallman64@gmail.com>
2023-05-20 19:22:48 -0400

    This should fix the google chrome 100vh url bar issue

--- components/app-page/add-item-modal-form.js
+++ components/app-page/add-item-modal-form.js
@@ -1,5 +1,5 @@
 //components/app-page/add-item-modal-form.js
-import { Fragment, useContext, useRef, useState } from "react";
+import { Fragment, useContext, useEffect, useRef, useState } from "react";
 import { ItemsContext } from "../../context/ItemsContext";
 import styles from "./modal-form.module.css";
 import { BiImageAdd } from "react-icons/bi";
@@ -15,6 +15,23 @@ function AddItemModalForm() {
     const [imageFileURL, setImageFileURL] = useState(itemBeforeEdit ? itemBeforeEdit.imageURL : null);
     const [imageFile, setImageFile] = useState(itemBeforeEdit ? itemBeforeEdit.imageFile : null);
     const defaultImagePath = "/default_image.png";
+
+    useEffect(() => {
+        function setVhVariable() {
+            let vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty('--vh', `${vh}px`);
+            console.log(vh);
+        }
+
+        // Run the function when the component mounts
+        setVhVariable();
+
+        // Run the function every time the window is resized
+        window.addEventListener('resize', setVhVariable);
+
+        // Clean up the event listener when the component unmounts
+        return () => window.removeEventListener('resize', setVhVariable);
+    }, []);
 
     const formik = useFormik({
         initialValues: {
